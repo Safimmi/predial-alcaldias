ddd71dd9b434876be63ad7c75b97b174fe48c5d3
safimmi <asztangie2@gmail.com>
2024-07-28 00:11:08 -0500

    feat : add predial receipt report route and controller

--- server/controllers/predialController.js
+++ server/controllers/predialController.js
@@ -1,4 +1,5 @@
 const Predial = require("../models/predial");
+const { generateReceipt } = require("../utils/reports");
 
 //TODO: Use "ficha" as the ID on the DB so it can be indexed => Use .findById() instead of .findOne()
 
@@ -60,7 +61,29 @@ const getPublicPredialById = async (req, res, next) => {
   }
 };
 
+const getPredialReceipt = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const query = { ficha: { $in: [id] } };
+    const predial = await Predial.findOne(query);
+
+    if (!predial) {
+      res.status(404);
+      throw new Error(`No se encontró un registro con el número de ficha </br> ${id}`);
+    }
+
+    const report = await generateReceipt(predial);
+
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'attachment; filename=predial.pdf');
+    res.status(200).end(report);
+  } catch (error) {
+    next(error);
+  };
+};
+
 module.exports = {
   getPredialById,
-  getPublicPredialById
+  getPublicPredialById,
+  getPredialReceipt
 };
--- server/routes/predialRoutes.js
+++ server/routes/predialRoutes.js
@@ -5,10 +5,27 @@ const authMiddleware = require('../middlewares/authMiddleware');
 const setPublicFlagMiddleware = require('../middlewares/setPublicFlagMiddleware');
 const validatePredialId = require('../middlewares/validatePredialId');
 
-
 const router = express.Router();
 
-router.get('/:id', authMiddleware, validatePredialId, predialController.getPredialById);
-router.get('/public/:id', setPublicFlagMiddleware, validatePredialId, predialController.getPublicPredialById);
+router.get(
+  '/:id',
+  authMiddleware,
+  validatePredialId,
+  predialController.getPredialById
+);
+
+router.get(
+  '/public/:id',
+  setPublicFlagMiddleware,
+  validatePredialId,
+  predialController.getPublicPredialById
+);
+
+router.get(
+  '/public/receipt/:id',
+  setPublicFlagMiddleware,
+  validatePredialId,
+  predialController.getPredialReceipt
+);
 
 module.exports = router;
